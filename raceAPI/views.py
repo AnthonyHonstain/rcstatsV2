aac489550cb1c40032aae115ab4f7ad9c0370514
Anthony Honstain <honstain@gmail.com>
2015-02-16 16:01:37 -0800

    Open the readonly API up to not require permissions

--- raceAPI/views.py
+++ raceAPI/views.py
@@ -2,17 +2,20 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework import mixins
 from rest_framework import generics
+from rest_framework.permissions import AllowAny
 
 from core.models import TrackName, SingleRaceDetails
 from raceAPI.serializers import TrackNameSerializer, SingleRaceDetailsSerializer
 
 
 class TrackNameList(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (AllowAny,)
     queryset = TrackName.objects.all()
     serializer_class = TrackNameSerializer
 
 
 class SingleRaceDetailsList(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (AllowAny,)
     queryset = SingleRaceDetails.objects.all()
     serializer_class = SingleRaceDetailsSerializer
 
