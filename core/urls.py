969fa9b00e0c31ec1ef4706e80e020fa386a2534
Anthony Honstain <honstain@gmail.com>
2016-04-30 17:27:54 -0700

    Initial launch of the king of the hill results, still need to debug and test, might be really really slow in prod

--- core/urls.py
+++ core/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns(
     url(r'racer-list/(?P<track_id>\d+)/$', views.racer_list, name='racer-list-by-track'),
     url(r'racer-list/(?P<track_id>\d+)/racerid/(?P<racerid_id>\d+)/$', views.single_racer_race_list, name='single_racer_race_list'),
     url(r'race-emails/$', views.race_emails, name='race-emails'),
+    url(r'king-of-the-hill/(?P<track_id>\d+)/$', views.king_of_the_hill_summary, name='koh-summary'),
 )
--- core/views.py
+++ core/views.py
@@ -5,6 +5,9 @@ from core.models import ClassEmailSubscription
 from django.db.models import Count
 from django.contrib.auth.decorators import login_required
 
+from django.utils import timezone
+from collections import defaultdict
+
 import logging
 logger = logging.getLogger('defaultlogger')
 
@@ -23,6 +26,9 @@ def index(request):
 
 
 def single_race_details(request, single_race_detail_id):
+    '''
+    Show the details for a single race.
+    '''
     logger.debug('metric=singleracedetail single_race_detail_id=%s', single_race_detail_id)
 
     single_race_detail = get_object_or_404(SingleRaceDetails, pk=single_race_detail_id)
@@ -37,6 +43,9 @@ def single_race_details(request, single_race_detail_id):
 
 
 def racer_list(request, track_id):
+    '''
+    Lists all the racers for a given track, along with a count of races.
+    '''
     logger.debug('metric=racer_list track_id=%s', track_id)
     trackname = get_object_or_404(TrackName, pk=track_id)
 
@@ -51,7 +60,7 @@ def racer_list(request, track_id):
       .annotate(racerid_count=Count('racerid'))\
       .order_by('-racerid_count')
 
-    return render(request, 'racer_list.html', {
+    return render(request, 'racer_list/racer_list.html', {
         'trackname': trackname, 
         'track_race_count': track_race_count,
         'tracks_first_race':tracks_first_race, 
@@ -59,6 +68,11 @@ def racer_list(request, track_id):
 
 
 def single_racer_race_list(request, track_id, racerid_id):
+    '''
+    For a track, list all of a single racer's results.
+    '''
+    logger.debug('metric=single_racer_race_list track_id=%s racerid_id=%s', track_id, racerid_id)
+
     trackname = get_object_or_404(TrackName, pk=track_id)
     racerid = get_object_or_404(RacerId, pk=racerid_id)
 
@@ -67,10 +81,69 @@ def single_racer_race_list(request, track_id, racerid_id):
         racerid__exact=racerid.id
         ).select_related('raceid').order_by('-raceid__racedate')
 
-    return render(request, 'single_racer_race_list.html', {
+    return render(request, 'racer_list/single_racer_race_list.html', {
         'trackname': trackname,
         'racerid': racerid,
         'races':races})
+
+
+class KoHSummary():
+    def __init__(self, official_class_name, racerid, score):
+        self.official_class_name = official_class_name
+        self.racerid = racerid
+        self.score = score
+    def __repr__(self):
+        return '{} {} {}'.format(
+            self.official_class_name.raceclass,
+            self.racerid.racerpreferredname,
+            self.score)
+
+
+def king_of_the_hill_summary(request, track_id):
+    trackname = get_object_or_404(TrackName, pk=track_id)
+
+    # We want a summary for the active classes.
+    class_names_data = OfficialClassNames.objects.filter(active=True)
+
+    now = timezone.now()
+    #utcnow = datetime.datetime.utcnow()
+    #utcnow.replace(tzinfo=pytz.utc)
+    two_weeks_ago = now - datetime.timedelta(days=14)
+
+    # So for this track, and these classes, we want to show the top performers.
+    race_summary = {}
+
+    # DANGER ZONE - slowness ahead
+    for class_name in class_names_data:
+        race_summary[class_name] = []
+        # What does that mean, just the racer with wins? Win count? Do I ignore second place?
+        race_results = SingleRaceResults.objects.filter(
+            raceid__trackkey__exact=trackname.id,
+            raceid__racedata__exact=class_name.raceclass,
+            raceid__racedate__gt=two_weeks_ago)\
+          .select_related('racerid').order_by('racerid')
+
+        MAX_SCORE = 21
+        racer_temp_dict = defaultdict(int)
+        for result in race_results:
+            #print('for racer', result.racerid.id, ' finished:', result.finalpos, ' score:', MAX_SCORE - result.finalpos)
+            racer_temp_dict[result.racerid] += MAX_SCORE - result.finalpos
+            #print('    ', racer_temp_dict[result.racerid])
+
+        for key in racer_temp_dict.keys():
+            summary = KoHSummary(class_name, key, racer_temp_dict[key])
+            #pprint.pprint(summary)
+            race_summary[class_name].append(summary)
+
+        race_summary[class_name].sort(key=lambda x: (x.official_class_name.raceclass, x.score), reverse=True)
+
+    #pprint.pprint(race_summary)
+    return render(request, 'king_of_the_hill/king_of_the_hill_summary.html', {
+        'trackname': trackname,
+        'start_time': two_weeks_ago,
+        'race_summary': race_summary,
+        })
+
 
 
 from django import forms
@@ -86,6 +159,9 @@ class EmailRaceListForm(forms.Form):
 
 @login_required()
 def race_emails(request):
+    '''
+    View and update all the classes a user is subscribed to.
+    '''
 
     logger.debug('metric=race_emails user=%s method=%s', request.user, request.method);
 
