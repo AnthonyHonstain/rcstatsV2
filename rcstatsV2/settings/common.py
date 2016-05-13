4801c3f06dd6503544630b116ff80f3974e13e15
Anthony Honstain <honstain@gmail.com>
2016-05-13 14:27:06 -0700

    Trying to modify the django-redis-cache config again to diagnose production redis bug

--- rcstatsV2/settings/common.py
+++ rcstatsV2/settings/common.py
@@ -178,7 +178,7 @@ CACHES = {
         'BACKEND': 'redis_cache.RedisCache',
         'LOCATION': os.environ.get('REDISTOGO_URL', 'redis://localhost:6379/0'),
         'OPTIONS': {
-            'DB': 1,
+            'DB': 0,
         },
     },
 }
